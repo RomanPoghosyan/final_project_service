46fc22ff61e160a19d093d045328de2d77d2147e
RomanPoghosyan <roman.poghos@gmail.com>
2020-03-03 01:25:42 +0400

    final changes

--- src/main/java/com/example/demo/services/NotificationService.java
+++ src/main/java/com/example/demo/services/NotificationService.java
@@ -1,25 +1,21 @@
 package com.example.demo.services;
 
-import com.example.demo.dto.requests.AdditionRequest;
-import com.example.demo.dto.requests.InviteRequest;
-import com.example.demo.dto.requests.NotificationStatusRequest;
-import com.example.demo.dto.requests.ReplyToInvitationRequest;
+import com.example.demo.dto.requests.*;
+import com.example.demo.dto.responses.FbNotificationResponse;
 import com.example.demo.dto.responses.NotificationResponse;
+import com.example.demo.dto.responses.OkResponse;
 import com.example.demo.exceptions.*;
 import com.example.demo.models.*;
 import com.example.demo.repos.NotificationRepository;
-import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.security.Principal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 
 @Service
@@ -55,21 +51,22 @@ public class NotificationService {
         notification.setProject(projectService.findById(invitedRequest.getProjectId()));
         notification.setInvitationStatus(InvitationStatus.PENDING);
         save(notification);
-        String apiKey = "AAAAoKWp3VM:APA91bFMjn87fPEUsUCtxJ4Loz0SsQ3GUb-1Al34ZoxaOP0x18ivCOhOTQhEhbg_YyYO790IOkzxwokjQuVs6AsbXuqD6guURCjEr_Qz8aAylndknxzMoyBnZWks7GR50IUEb0X5fT7L";
-        URL url = new URL("https://fcm.googleapis.com/fcm/send");
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("POST");
-        conn.setRequestProperty("Content-Type", "application/json");
-        conn.setRequestProperty("Authorization", "key=" + apiKey);
-        JSONObject notificationJSON = new JSONObject();
-        notificationJSON.put("title", "Invitation to the project!");
-        String notificationBody = notification.getNotifiedBy().getUsername() + " has invited you to the " + notification.getProject().getName() + "project!";
-        notificationJSON.put("body", notificationBody);
-        JSONObject message = new JSONObject();
-        message.put ("to", notification.getNotifiedTo().getFb_token());
-        message.put ("priority", "high");
-        message.put("notification", notificationJSON);
-        conn.setRequestProperty("body", message.toString());
+        firebaseMessagingService.sendInvitationMessage(notification);
+        return notification;
+    }
+
+    public Notification assignTask(AssignTaskRequest assignTaskRequest, Principal principal) throws ProjectNotFound, UserNotFound, IOException, TaskNotFound {
+        User assignor = userService.findByUsername(principal.getName());
+        User assignee = userService.findById(assignTaskRequest.getAssigneeId());
+        Notification notification = new Notification();
+        notification.setType(NotificationType.ASSIGNING);
+        notification.setStatus(NotificationStatus.NOT_SEEN);
+        notification.setNotifiedTo(assignee);
+        notification.setNotifiedBy(assignor);
+        notification.setTask(taskService.findById(assignTaskRequest.getTaskId()));
+        notification.setProject(projectService.findById(assignTaskRequest.getProjectId()));
+        save(notification);
+        firebaseMessagingService.sendAssignmentNotification(notification);
         return notification;
     }
 
